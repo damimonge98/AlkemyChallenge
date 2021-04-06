d275e72d60e979a691ed65c3bc20fd9c46e0254b
damimonge98 <69829575+damimonge98@users.noreply.github.com>
2021-04-06 17:30:09 -0300

    CLIENT: filter categories

--- client/src/components/Operations/LastOperations/LastOperations.jsx
+++ client/src/components/Operations/LastOperations/LastOperations.jsx
@@ -1,14 +1,17 @@
-import React, {useState, useEffect } from "react";
+import React, {useState, useEffect, Fragment } from "react";
 import "./lastoperations.css";
 import axios from "axios";
 import { FaPlaneDeparture, FaTasks, FaPizzaSlice, FaClinicMedical, FaShoppingCart, FaBus, FaRegSmileBeam, FaFilter, FaBell} from 'react-icons/fa';
 import {FontAwesome, FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFilter} from "@fortawesome/free-solid-svg-icons";
-import Button from "../../Button/Button";
 
 export default function Operations () {
     const [categories, setCategories] = useState([]);
     const [operation, setOperation] = useState(0);
+    const [filter, setFilter] = useState(false);
+    const [filterCategories, setFilterCategories] = useState([]);
+    const [check, setCheck] = useState(false)
+    const [id, setCategoryId] = useState([])
 
     useEffect (()=> {
         axios.get("/operations")
@@ -18,22 +21,71 @@ export default function Operations () {
         .then (categories => setCategories(categories.data))
     },[])
 
-    var lastOperations = []
+    //Handlers para filtrado de categorías
+    const filterHandler = function () {
+        setFilter(!filter)
+    }
+
+    const filterCategoriesHandler = function (id) {
+
+            if (filterCategories.includes(id)) {
+                setCheck(!check)
+
+                console.log("check inside", check)
+                var spliceCategories = filterCategories
+                spliceCategories.splice(spliceCategories.indexOf(id), 1)
+                setFilterCategories(spliceCategories)
+                return;
+            } 
+            else {
+                setFilterCategories([...filterCategories, id])
+                return;
+            }
+        }
+
+        
     
+    //Manejo de ultimas 10 operaciones
+    var lastOperations = []
     var lastOperations = operation!== 0? operation.data: []
     lastOperations = lastOperations.length >= 10 ? lastOperations.slice(operation.data.length-10, operation.data.length) : lastOperations
     lastOperations = lastOperations.reverse()
 
+    //Condicional para filtrar 
+    if (filterCategories.length !== 0) {
+        var newArray = []
+        for (var i = 0; i < filterCategories.length; i ++) {
+        lastOperations.filter(el => el.categoryId === filterCategories[i]).map(el => newArray.push(el))
+        }
+        lastOperations = newArray;
+    }
+
+    console.log("filterCategories", filterCategories);
     console.log("lastOperations", lastOperations)
     console.log("categories", categories)
-    console.log("operations", operation)
+    console.log("check fuera", check)
 
     return (<div className = "containerLastOperationsDiv">
 
             <div className = "headerDiv">
             <h3 className = "h3title">TUS ÚLTIMAS OPERACIONES:</h3>
-            <FontAwesomeIcon icon = {faFilter} size = "lg" className = "filterIcon"/>
+            <FontAwesomeIcon icon = {faFilter} size = "lg" className = "filterIcon" onClick = {filterHandler}/>
             </div>
+
+            {filter === false? null : 
+            <Fragment>
+                <div>
+                    {categories.length === 0? null 
+                    : categories.map(el => 
+                        <Fragment>
+                        <input type="checkbox" onClick = {()=>{filterCategoriesHandler(el.id)}}/>
+                        <label>{el.name.charAt(0).toUpperCase() + el.name.substring(1)}</label>
+                        </Fragment>
+                   )}
+                    
+                </div>
+            </Fragment>}
+
 
             {lastOperations.length !== 0 ?
             lastOperations.map(el => 
